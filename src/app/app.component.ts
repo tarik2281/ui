b92088433b927863b76c825c8e8876a9ea02eac6
Tarik Karaca <19tarik97@gmail.com>
2020-01-24 21:45:33 +0100

    replace scrollListener with IntersectionObserver and sticky header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MatSnackBar } from '@angular/material';
 import { LoginDialogComponent } from 'src/app/components/login-dialog/login-dialog.component';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { User } from 'src/app/model/user';
 import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 import { NavigationEnd, Router } from '@angular/router';
+import { animate, AnimationEvent, style, transition, trigger } from '@angular/animations';
 
 const SCROLL_OFFSET = 384;
 
@@ -13,9 +14,22 @@ const APP_LOGO_HEIGHT = 192;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('stickyLogo', [
+      transition(':enter', [
+        style({ opacity: 1, height: '192px', top: '-192px', left: '100px' }),
+        animate('.25s ease', style({ opacity: 1, height: '64px', top: 0, left: '16px' }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1, height: '64px', top: 0, left: '16px' }),
+        animate('.25s ease', style({ opacity: 1, height: '192px', top: '-192px', left: '100px' }))
+      ])
+    ])
+
+  ]
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'ui';
 
   user: User;
@@ -32,8 +46,12 @@ export class AppComponent implements OnInit {
 
   fancyTextAlpha = 0.87;
 
-  @ViewChild('toolbarDom', { static: false }) toolbarDom: ElementRef<HTMLElement>;
+  logoVisible = true;
+
+  @ViewChild('toolbarDom', { read: ElementRef, static: false }) toolbarDom: ElementRef<HTMLElement>;
   @ViewChild('content', { static: false }) contentDiv: ElementRef<HTMLDivElement>;
+  @ViewChild('mainContainer', { static: false }) mainContainer: ElementRef<HTMLDivElement>;
+  @ViewChild('stickySentinel', { static: false }) stickySentinel: ElementRef<HTMLDivElement>;
 
   constructor(private authenticationService: AuthenticationService,
               private dialog: MatDialog,
@@ -49,9 +67,40 @@ export class AppComponent implements OnInit {
     });
 
     this.authenticationService.me();
+
+  }
+
+  ngAfterViewInit(): void {
+    // console.log(this.toolbarDom)
+
+    const obs = new IntersectionObserver((entries, observer) => {
+      console.log(entries);
+
+      if (entries[0].isIntersecting) {
+        this.isAdded = false;
+      } else {
+        this.isAdded = true;
+        this.logoVisible = false;
+      }
+      // for (const entry of entries) {
+      //
+      // }
+    });
+
+    obs.observe(this.stickySentinel.nativeElement);
+    // obs.observe(this.stickySentinel.nativeElement);
   }
 
   firstActivation = true;
+
+  onAnimationDone(event: AnimationEvent) {
+    console.log(event);
+
+    if (event.toState === 'void') {
+      console.log('hidden');
+      this.logoVisible = true;
+    }
+  }
 
   onActivate(event) {
     if (this.firstActivation) {
@@ -61,9 +110,9 @@ export class AppComponent implements OnInit {
 
     console.log(event);
     if (!this.isAdded) {
-      window.scrollTo({behavior: 'smooth', top: 458});
+      window.scrollTo({ behavior: 'smooth', top: 458 });
     } else {
-      window.scrollTo({ behavior: 'auto', top: 458});
+      window.scrollTo({ behavior: 'auto', top: 458 });
     }
   }
 
@@ -91,27 +140,27 @@ export class AppComponent implements OnInit {
 
   @HostListener('window:scroll', [])
   onAppScroll() {
-    const offsetY = window.scrollY - this.lastScrollY;
-
-    this.positionY = Math.max(-window.scrollY, -SCROLL_OFFSET);
-
-    if (this.positionY <= -SCROLL_OFFSET && !this.isAdded) {
-      this.isAdded = true;
-      // this.toolbarDom.nativeElement.classList.add('mat-elevation-z4');
-    }
-
-    if (this.positionY > -SCROLL_OFFSET && this.isAdded) {
-      this.isAdded = false;
-      // this.toolbarDom.nativeElement.classList.remove('mat-elevation-z4');
-    }
-
-    // this.logoHeight = lerp(128, 64, -this.positionY / SCROLL_OFFSET)
-    this.logoY = this.positionY + (SCROLL_OFFSET - APP_LOGO_HEIGHT);
-    // this.logoX = lerp(100, 10, -this.positionY / SCROLL_OFFSET);
-    // this.fancyTextAlpha = lerp(0.87, 0.0, -this.positionY / SCROLL_OFFSET);
-
-    // this.positionY = Math.min(0, Math.max(this.positionY - offsetY, -128));
-    this.lastScrollY = window.scrollY;
+    // const offsetY = window.scrollY - this.lastScrollY;
+    //
+    // this.positionY = Math.max(-window.scrollY, -SCROLL_OFFSET);
+    //
+    // if (this.positionY <= -SCROLL_OFFSET && !this.isAdded) {
+    //   this.isAdded = true;
+    //   // this.toolbarDom.nativeElement.classList.add('mat-elevation-z4');
+    // }
+    //
+    // if (this.positionY > -SCROLL_OFFSET && this.isAdded) {
+    //   this.isAdded = false;
+    //   // this.toolbarDom.nativeElement.classList.remove('mat-elevation-z4');
+    // }
+    //
+    // // this.logoHeight = lerp(128, 64, -this.positionY / SCROLL_OFFSET)
+    // this.logoY = this.positionY + (SCROLL_OFFSET - APP_LOGO_HEIGHT);
+    // // this.logoX = lerp(100, 10, -this.positionY / SCROLL_OFFSET);
+    // // this.fancyTextAlpha = lerp(0.87, 0.0, -this.positionY / SCROLL_OFFSET);
+    //
+    // // this.positionY = Math.min(0, Math.max(this.positionY - offsetY, -128));
+    // this.lastScrollY = window.scrollY;
   }
 
   toggleVisibility() {
@@ -120,5 +169,5 @@ export class AppComponent implements OnInit {
 }
 
 function lerp(start, end, amt) {
-  return (1-amt)*start+amt*end;
+  return (1 - amt) * start + amt * end;
 }
